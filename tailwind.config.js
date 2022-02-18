f6b4362bd2ef245a7fcb075cb0ca97b333b01700
study-hary-id <haryansyah.study.id@gmail.com>
2022-02-18 19:12:57 +0700

    Create NextJS project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '.55rem'
+      }
+    },
+  },
+  plugins: [],
+}
